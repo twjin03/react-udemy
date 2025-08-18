26299188a12bccccaaeccdd5c95c1d5ce750523f
twjin03 <twjindev@gmail.com>
2025-08-19 02:51:54 +0900

    43. Prop으로 컴포넌트 재사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentImg from './assets/components.png';
+
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
-function genRandomInt(max){
-  return Math.floor(Math.random() * (max +1)); 
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
@@ -20,13 +22,38 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 
 function App() {
   return (
     <div>
-      <Header/> 
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept
+            title="Components"
+            description="The core UI building block."
+            image={componentImg}
+          />
+          <CoreConcept
+            title="Props"
+          />
+          <CoreConcept />
+          <CoreConcept />
+        </ul>
+        </section>
+        
       </main>
     </div>
   );
