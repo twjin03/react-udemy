0408b292982bcc27d07bed0dbb17119f6709a2ea
twjin03 <twjindev@gmail.com>
2025-08-19 13:58:55 +0900

    50. 함수를 Prop의 값으로 전달하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import CoreConcept from './components/CoreConcept';
 import Header from './components/Header/Header.jsx';
+import TabButton from './components/TabButton.jsx';
 import { CORE_CONCEPTS } from './data';
 
 
 function App() {
+  function handleSelect() {
+    console.log('Hello World! - selected');
+  }
+
   return (
     <div>
       <Header />
@@ -20,6 +25,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
