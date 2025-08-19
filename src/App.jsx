7c79905a2b80872462e9ebc6d8e232f7c085ddf2
twjin03 <twjindev@gmail.com>
2025-08-19 18:00:08 +0900

    54. 데이터 기반 State 가져오기 및 출력

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'; 
+import { useState } from 'react';
 
 import CoreConcept from './components/CoreConcept';
 import Header from './components/Header/Header.jsx';
 import TabButton from './components/TabButton.jsx';
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton); 
+    setSelectedTopic(selectedButton);
     // console.log(selectedTopic);
   }
 
@@ -34,12 +34,20 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => {handleSelect('components')}}>Components</TabButton>
+            <TabButton onSelect={() => { handleSelect('components') }}>Components</TabButton>
             <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
