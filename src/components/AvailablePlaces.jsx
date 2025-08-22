0902baf454942f167577b580bb91fa1b6f6856ae
twjin03 <twjindev@gmail.com>
2025-08-22 16:58:32 +0900

    style: async/await 추가

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -6,14 +6,12 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    
-    fetch('http://localhost:3000/places')
-      .then((response) => {
-        return response.json() // json 형식의 데이터 추출 - 텍스트 기반 데이터 형식임 
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      }); // 브라우저가 제공함 
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places'); 
+      const resData = await response.json(); 
+      setAvailablePlaces(resData.places);
+    }
+    fetchPlaces(); 
   }, []); // 의존성 바뀌었다는 전제 하에 실행됨 
   // 빈칸이면 딱 한번 실행됨 -> 무한 루프 빠지지 않음 
   return (
