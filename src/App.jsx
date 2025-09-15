de3ebffed62ff66bc52871b884a63300c1c46d3e
twjin03 <twjindev@gmail.com>
2025-09-15 16:11:29 +0900

    fix: 중복 상태관리 제거 및 헬퍼 함수 정의

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 
+// 헬퍼 함수 
+function deriveActivePlayer(gameTurns) {
+  let currnetPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currnetPlayer = 'O';
+  }
+  return currnetPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState('X');
 
-  // 플레이어 토글 기능 수행하는 함수
+  const activePlayer = deriveActivePlayer(gameTurns);
+
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns(prevTurns => {
-      let currnetPlayer = 'X';
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currnetPlayer = 'O';
-      }
+      const currnetPlayer = deriveActivePlayer(prevTurns);
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: activePlayer },
         ...prevTurns];
@@ -29,12 +36,12 @@ function App() {
           <Player initialName="player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard 
-        onSelectSquare={handleSelectSquare} 
-        turns={gameTurns} />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns} />
       </div>
 
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main >
   );
 }
