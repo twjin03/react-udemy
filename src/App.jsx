e24c3fcdaf380994fdd4fa16df03bd013df0201f
twjin03 <twjindev@gmail.com>
2025-09-21 21:26:39 +0900

    feat: 현재 선택 상태를 버튼으로 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
+import { NoProjectSelected } from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <>
-      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <main className="h-screen my-8 flex gap-8">
-        <ProjectSidebar />
-        <NewProject />
+        <ProjectSidebar onStartAddProject={handleStartAddProject} />
+        {content}
       </main>
     </>
   );
