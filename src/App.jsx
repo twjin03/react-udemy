e74c92af3c9bf6ac644846f4b357d8cdfdd9bd23
twjin03 <twjindev@gmail.com>
2025-09-25 00:29:05 +0900

    feat: useFetch 커스텀 훅

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,16 @@ import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import { fetchUserPlaces, updateUserPlaces } from './http.js';
 import ErrorData from './components/Error.jsx';
-
+import { useFetch } from './hooks/useFetch.js';
 
 function App() {
   const selectedPlace = useRef();
-
-  const [userPlaces, setUserPlaces] = useState([]);
-
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
 
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        const places = await fetchUserPlaces();
-        setUserPlaces(places);
-      } catch (error) {
-        setError({ message: error.nessage || 'Failed to fetch user places.' })
-      }
-      setIsFetching(false);
-    }
-    fetchPlaces();
-  }, []);
+  const {isFetching, error, fetchedData: userPlaces} = useFetch(fetchUserPlaces, []);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -44,43 +27,43 @@ function App() {
     setModalIsOpen(false);
   }
 
-  async function handleSelectPlace(selectedPlace) {
+  // async function handleSelectPlace(selectedPlace) {
 
-    setUserPlaces((prevPickedPlaces) => { // state 업데이트 말고 백엔드로 바로 보내기 
-      if (!prevPickedPlaces) {
-        prevPickedPlaces = [];
-      }
-      if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
-        return prevPickedPlaces;
-      }
-      return [selectedPlace, ...prevPickedPlaces];
-    });
+  //   setUserPlaces((prevPickedPlaces) => { // state 업데이트 말고 백엔드로 바로 보내기 
+  //     if (!prevPickedPlaces) {
+  //       prevPickedPlaces = [];
+  //     }
+  //     if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
+  //       return prevPickedPlaces;
+  //     }
+  //     return [selectedPlace, ...prevPickedPlaces];
+  //   });
 
-    try {
-      // updateUserPlaces(userPlaces);  // 상태 업데이트가 즉각적으로 이 코드에서 이뤄지지 않음 (스케줄링만 함) 
-      await updateUserPlaces([selectedPlace, ...userPlaces]); // 새 배열에 추출하기 
-    } catch (error) {
-      setUserPlaces(userPlaces);
-      setErrorUpdatingPlaces({ message: error.message || 'Failed to update places.', }); // 에러 메시지 상태 set 
-    }
+  //   try {
+  //     // updateUserPlaces(userPlaces);  // 상태 업데이트가 즉각적으로 이 코드에서 이뤄지지 않음 (스케줄링만 함) 
+  //     await updateUserPlaces([selectedPlace, ...userPlaces]); // 새 배열에 추출하기 
+  //   } catch (error) {
+  //     setUserPlaces(userPlaces);
+  //     setErrorUpdatingPlaces({ message: error.message || 'Failed to update places.', }); // 에러 메시지 상태 set 
+  //   }
 
 
-  }
+  // }
 
-  const handleRemovePlace = useCallback(async function handleRemovePlace() {
-    setUserPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
-    );
+  // const handleRemovePlace = useCallback(async function handleRemovePlace() {
+  //   setUserPlaces((prevPickedPlaces) =>
+  //     prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
+  //   );
 
-    try {
-      await updateUserPlaces(userPlaces.filter(place => place.id !== selectedPlace.current.id));
-    } catch (error) {
-      setUserPlaces(userPlaces);
-      setErrorUpdatingPlaces({ message: error.message || 'Failed to delete place.', });
-    }
+  //   try {
+  //     await updateUserPlaces(userPlaces.filter(place => place.id !== selectedPlace.current.id));
+  //   } catch (error) {
+  //     setUserPlaces(userPlaces);
+  //     setErrorUpdatingPlaces({ message: error.message || 'Failed to delete place.', });
+  //   }
 
-    setModalIsOpen(false);
-  }, [userPlaces]); // userPlaces 상태가 바뀌면 이 함수가 꼭 재생성되도록 해야 함 
+  //   setModalIsOpen(false);
+  // }, [userPlaces]); // userPlaces 상태가 바뀌면 이 함수가 꼭 재생성되도록 해야 함 
   function handleError() {
     setErrorUpdatingPlaces(null); // 에러 메시지 출력한 후 에러 없애기 위함 
   }
@@ -98,7 +81,7 @@ function App() {
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
+          // onConfirm={handleRemovePlace}
         />
       </Modal>
 
@@ -111,7 +94,7 @@ function App() {
         </p>
       </header>
       <main>
-        {error && <ErrorData title="An error occured!" message={error.message}/>}
+        {error && <ErrorData title="An error occured!" message={error.message} />}
         {!error && <Places
           title="I'd like to visit ..."
           fallbackText="Select the places you would like to visit below."
@@ -121,7 +104,9 @@ function App() {
           onSelectPlace={handleStartRemovePlace}
         />}
 
-        <AvailablePlaces onSelectPlace={handleSelectPlace} />
+        <AvailablePlaces 
+        // onSelectPlace={handleSelectPlace} 
+        />
       </main>
     </>
   );
