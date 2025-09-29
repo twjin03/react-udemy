bfa808680629aa3d46cdead3284dd205c14dfa4e
twjin03 <twjindev@gmail.com>
2025-09-29 23:55:43 +0900

    feat: Ref 사용할 경우의 validation
    제출 유효성 검사 로직 구현

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,27 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from 'react';
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
-  function handleSubmit(e) {
-    e.preventDefault(); // 기본 동작 막기 
-    // console.log(enteredValues);
-    const enteredEmail = email.current .value; 
-    const enteredPassword = password.current .value; 
-    console.log(enteredEmail, enteredPassword);
-    // email.current.value = ''; -> 추천되지 않음 ! 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log('Sending HTTP request...');
   }
 
   return (
@@ -20,28 +31,21 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            ref={email}
-          />
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            ref={password}
-          />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button" >Login</button>
+        <button className="button">Login</button>
       </p>
     </form>
   );
