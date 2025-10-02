47fb48aed8e9971222fda261be07720ac6b24e62
twjin03 <twjindev@gmail.com>
2025-10-02 17:43:50 +0900

    feat: useParams 사용하여 동적 라우트 정의

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,3 +1,10 @@
 export default function ProductsPage() {
-return <h1>The products page</h1>;
+  return <>
+    <h1>The products page</h1>
+    <ul>
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+    </ul>
+  </>;
 }
