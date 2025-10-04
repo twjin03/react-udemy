76fb656fb9b26b67cdddd79472aac24bd4425b40
twjin03 <twjindev@gmail.com>
2025-10-05 04:07:37 +0900

    refactor: TodoItem 컴포넌트 분리

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
 // Functional Component 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return <ul>
     {props.items.map(
-      item => (<li key={item.id}>{item.text}</li>))}
+      item => (<TodoItem key={item.id} text={item.text} />))}
   </ul>
 }
 
