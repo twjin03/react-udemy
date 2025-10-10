764a2a3e6b4c3acb1377ae29fc188aebf3ee57a2
twjin03 <twjindev@gmail.com>
2025-10-10 21:16:47 +0900

    refactor: React Query 적용하기

--- src/components/Events/EventItem.jsx
+++ src/components/Events/EventItem.jsx
@@ -8,7 +8,7 @@ export default function EventItem({ event }) {
   });
   return (
     <article className="event-item">
-      <img src={`http://localhost:3000/${event.image}`} alt={event.title} />
+      <img src={`http://localhost:3333/${event.image}`} alt={event.title} />
       <div className="event-item-content">
         <div>
           <h2>{event.title}</h2>
