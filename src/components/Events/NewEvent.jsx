581b62384c784e7758d0143220cf1fca850653bc
twjin03 <twjindev@gmail.com>
2025-10-11 00:37:52 +0900

    feat: 새 이벤트 post 성공 후 데이터 및 ui 자동으로 업데이트하기

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -3,14 +3,18 @@ import { useMutation } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { createNewEvent } from '../../utils/http.js';
+import { createNewEvent, queryClient } from '../../utils/http.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
   const { mutate, isPending, isError, error } = useMutation({
-    mutationFn: createNewEvent
+    mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['events'] });
+      navigate('/events');
+    }
   });
 
   function handleSubmit(formData) {
@@ -30,7 +34,7 @@ export default function NewEvent() {
           </button>
         </>)}
       </EventForm>
-      {isError && <ErrorBlock  title="새로운 이벤트를 생성하는 데 실패했습니다." message={error.info?.message || '이벤트 생성에 실패했습니다. 입력 정보를 다시 확인하거나 나중에 다시 시도해주세요.'}/>}
+      {isError && <ErrorBlock title="새로운 이벤트를 생성하는 데 실패했습니다." message={error.info?.message || '이벤트 생성에 실패했습니다. 입력 정보를 다시 확인하거나 나중에 다시 시도해주세요.'} />}
     </Modal>
   );
 }
