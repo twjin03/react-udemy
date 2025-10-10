39edd5364f79524ea928e4e6c28c7803ef065449
twjin03 <twjindev@gmail.com>
2025-10-11 02:35:28 +0900

    feat: 이벤트 편집 기능 구현하기
    - input 창에 기존 데이터 표시하기
    - useMutation 사용해서 PUT 요청 처리하기

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,27 +1,68 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useQuery, useMutation } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { fetchEvent, updateEvent } from '../../utils/http.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
 
-  function handleSubmit(formData) {}
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate('../');
+  }
 
   function handleClose() {
     navigate('../');
   }
 
+  let content;
+
+  if (isPending) {
+    content = (<div className='center'><LoadingIndicator /></div>);
+  }
+
+  if (isError) {
+    content = (<>
+      <ErrorBlock
+        title="이벤트 불러오기 실패"
+        message={error.info?.message || "이벤트를 가져오는 데 실패했습니다. 입력한 값을 확인하거나 나중에 다시 시도해주세요."}
+      />
+      <div className='form-actions'>
+        <Link to="../" className='button'>
+          확인
+        </Link>
+      </div>
+    </>);
+  }
+
+  if (data) {
+    content = (<EventForm inputData={data} onSubmit={handleSubmit}>
+      <Link to="../" className="button-text">
+        Cancel
+      </Link>
+      <button type="submit" className="button">
+        Update
+      </button>
+    </EventForm>);
+  }
+
   return (
     <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
+      {content}
     </Modal>
   );
 }
