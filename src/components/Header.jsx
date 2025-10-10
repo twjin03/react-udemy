e7da6c299284a0e3bd6d8e41d7eec77df28f3d27
twjin03 <twjindev@gmail.com>
2025-10-11 05:17:22 +0900

    fix: react-router를 react-query와 함께 사용하기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { useIsFetching } from "@tanstack/react-query";
+
 export default function Header({ children }) {
+  const fetching = useIsFetching();
   return (
     <>
       <div id="main-header-loading"></div>
+      {fetching > 0 && <progress />}
       <header id="main-header">
         <div id="header-title">
           <h1>React Events</h1>
