b802ac365fe568296216b3d5b1f6c479adeb72b9
twjin03 <twjindev@gmail.com>
2025-10-11 01:35:43 +0900

    feat: 이벤트 id에 따라 세부 내용 데이터 불러오기
    + 날짜 포매팅

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,63 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
 import Header from '../Header.jsx';
+// import { useMutation, useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../../utils/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { useQuery } from '@tanstack/react-query';
 
 export default function EventDetails() {
+  const params = useParams();
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
+  // const { } = useMutation();
+
+  let content;
+
+  if (isPending) {
+    content = <div id="event-details-conntent" className='center'>
+      <p>데이터를 불러오는 중...</p>
+    </div>
+  }
+
+  if (isError) {
+    content = <div id="event-details-conntent" className='center'>
+      <ErrorBlock title="이벤트를 가져오는 데 실패함." message={error.info?.message || '이벤트 데이터를 가져오는 데 실패했습니다. 나중에 다시 시도해주세요.'} />
+    </div>
+  }
+
+  if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString('ko-KR', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric'
+    });
+
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3333/${data.image}`} alt={data.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>{formattedDate} @ {data.time}</time>
+            </div>
+            <p id="event-details-description">{data.discription}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <Outlet />
@@ -12,23 +67,7 @@ export default function EventDetails() {
         </Link>
       </Header>
       <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
+        {content}
       </article>
     </>
   );
