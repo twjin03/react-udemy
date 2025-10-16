5552f6061ce0de23b370dd66ea617ecde2fa1c92
twjin03 <twjindev@gmail.com>
2025-10-17 03:03:36 +0900

    refactor: loader 함수를 컴포넌트 안에서 관리

--- router-with-backend/frontend/src/App.js
+++ router-with-backend/frontend/src/App.js
@@ -22,7 +22,7 @@
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import EventsPage from './pages/EventsPage';
+import EventsPage, { loader as eventsLoader } from './pages/EventsPage';
 import EventDetailPage from './pages/EventDetailPage';
 import NewEventPage from './pages/NewEventPage';
 import EditEventPage from './pages/EditEventPage';
@@ -38,16 +38,7 @@ const router = createBrowserRouter([
         path: 'events', element: <EventsRootLayout />, children: [
           {
             index: true, element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch('http://localhost:8080/events');
-
-              if (!response.ok) {
-                // ...
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            }
+            loader: eventsLoader,
           },
           { path: ':eventId', element: <EventDetailPage /> },
           { path: 'new', element: <NewEventPage /> },
--- router-with-backend/frontend/src/pages/EventsPage.js
+++ router-with-backend/frontend/src/pages/EventsPage.js
@@ -10,3 +10,17 @@ export default function EventsPage() {
     </>
   );
 }
+
+
+export async function loader() {
+
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    // ...
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+
+}
