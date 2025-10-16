0ffff4ea56cabf212c3c55b62d41c1840826ed0b
twjin03 <twjindev@gmail.com>
2025-10-17 01:01:46 +0900

    feat: 초기 라우팅 페이지 정의 및 RootLayout 설정

--- router-with-backend/frontend/src/App.js
+++ router-with-backend/frontend/src/App.js
@@ -20,8 +20,30 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import EventsPage from './pages/EventsPage';
+import EventDetailPage from './pages/EventDetailPage';
+import NewEventPage from './pages/NewEventPage';
+import EditEventPage from './pages/EditEventPage';
+import RootLayout from './pages/Root';
+
+const router = createBrowserRouter([
+  {
+    path: '/', element: <RootLayout />, children: [
+      { index: true, element: <HomePage /> },
+      { path: '/events', element: <EventsPage /> },
+      { path: '/events/:eventId', element: <EventDetailPage /> },
+      { path: '/events/new', element: <NewEventPage /> },
+      { path: '/:eventId/edit', element: <EditEventPage /> },
+    ]
+  },
+
+]);
+
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
