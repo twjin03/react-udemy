8d4210d27ad740da0b0552d427afe1503ac17815
twjin03 <twjindev@gmail.com>
2025-10-17 03:14:46 +0900

    fix: 일부 코드 로직 수정
    참고) loader()에서는 브라우저 기능 모두를 사용할 수 있음
    예: localstorage, cookie 사용 등

--- router-with-backend/frontend/src/pages/EventsPage.js
+++ router-with-backend/frontend/src/pages/EventsPage.js
@@ -2,7 +2,8 @@ import EventsList from '../components/EventsList';
 import { useLoaderData } from 'react-router-dom';
 
 export default function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
 
   return (
     <>
@@ -13,14 +14,13 @@ export default function EventsPage() {
 
 
 export async function loader() {
-
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     // ...
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
+    // react router가 자동으로 데이터를 추출하기 때문에 따로 추가 작업할 필요 없이 바로 반환하는 게 가능함
   }
 
 }
