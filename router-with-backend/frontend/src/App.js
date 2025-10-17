98e57128583e735c36b231b2063684397382a921
twjin03 <twjindev@gmail.com>
2025-10-17 15:44:15 +0900

    feat: action() 사용해서 폼 제출 처리하기
    <from>이 아닌 react router에서 제공하는 <Form>을 사용
    -> default event를 막고, 백엔드에 데이터 보내기 전에 일단 action()으로 데이터를 넘김

--- router-with-backend/frontend/src/App.js
+++ router-with-backend/frontend/src/App.js
@@ -24,7 +24,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import EventsPage, { loader as eventsLoader } from './pages/EventsPage';
 import EventDetailPage, { loader as eventDetailLoader } from './pages/EventDetailPage';
-import NewEventPage from './pages/NewEventPage';
+import NewEventPage, { action as newEventAction } from './pages/NewEventPage';
 import EditEventPage from './pages/EditEventPage';
 import RootLayout from './pages/Root';
 import EventsRootLayout from './pages/EventsRoot';
@@ -53,7 +53,7 @@ const router = createBrowserRouter([
               { path: 'edit', element: <EditEventPage /> }
             ]
           },
-          { path: 'new', element: <NewEventPage /> },
+          { path: 'new', element: <NewEventPage />, action: newEventAction },
         ]
       }
     ]
--- router-with-backend/frontend/src/pages/NewEventPage.js
+++ router-with-backend/frontend/src/pages/NewEventPage.js
@@ -1,3 +1,32 @@
+import { redirect } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
 export default function NewEventPage() {
-  return <h1>NewEventPage</h1>;
+  return <EventForm />;
+}
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+
+  // 필드에 설정한 name으로 값 가져옴 
+  const eventData = {
+    title: data.get('title'),
+    image: data.get('image'),
+    date: data.get('date'),
+    description: data.get('description'),
+  }
+  const response = await fetch('http://localhost:8080/events', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: 'Could not save event.' }),
+      { status: 500 });
+  }
+
+  return redirect('/events');
 }
