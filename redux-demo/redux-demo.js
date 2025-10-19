4bf6c3176f5f0daeeed3d1281dfed3657d86fb5d
twjin03 <twjindev@gmail.com>
2025-10-19 18:56:01 +0900

    redux 핵심 코드 및 작동 방식 (without React)

---
+++ redux-demo/redux-demo.js
@@ -0,0 +1,34 @@
+const redux = require('redux');
+
+// reducer function
+// should be a pure function 
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1
+    };
+  }
+  return state;
+};
+
+// 저장소 만들기
+const store = redux.createStore(counterReducer);
+
+// console.log(store.getState());
+
+// subscriper 
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });
+
