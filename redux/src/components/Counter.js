3d688a75f30d08799e6f9601a351446f56e48ddc
twjin03 <twjindev@gmail.com>
2025-10-21 11:37:26 +0900

    feat: useSelector 사용해서 store 구독 및 데이터 사용

--- redux/src/components/Counter.js
+++ redux/src/components/Counter.js
@@ -1,12 +1,16 @@
 import classes from './Counter.module.css';
+import { useSelector } from 'react-redux';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector(state => state.counter); // 이때 자동으로 이 컴포넌트는 저장소를 구독
+  // 컴포넌트 제거하면 자동으로 구독 삭제
+
+  const toggleCounterHandler = () => { };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
