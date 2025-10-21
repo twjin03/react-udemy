273244749f340d4a0ba2472c5fedd85b301d67eb
twjin03 <twjindev@gmail.com>
2025-10-21 18:10:55 +0900

    feat: 여러 컴포넌트에서 action dispatch

--- redux/src/App.js
+++ redux/src/App.js
@@ -2,14 +2,17 @@ import { Fragment } from 'react/jsx-runtime';
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import { useSelector } from 'react-redux';
 
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
   return (
     <Fragment >
       <Header />
-      <Auth />
-      <Counter />
+      {!isAuth && <Auth />}
+      {isAuth && <Counter />}
     </Fragment>
 
   );
